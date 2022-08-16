b6a973a2263477e868e663869679b8d99a10beae
Wagner Christos <88002748+wagnerchr@users.noreply.github.com>
2022-08-16 11:37:41 -0300

    Gera sequência de  N números aleatórios
    Classe que cria N pastas com N arquivos ".txt" com N números aleatórios

---
+++ JavaFiles/Ex02.java
@@ -0,0 +1,11 @@
+public class Ex02 {
+    
+    public static void main(String[] args) {
+        GeraSequencias geraSeq = new GeraSequencias();
+
+
+    // Cria Pastas com Sequências
+        // (Número de arquivos .txt, Quantidade de Números aleatórios, Array(Quantidade Pastas))
+        geraSeq.geraSequencias(10,  500, 100, 1000, 10000, 50000, 100000);
+    }
+}
---
+++ JavaFiles/OrdenaSequencia.java
@@ -0,0 +1,5 @@
+
+// Classe para ordenar os arquivos .txt criados pela Classe GeraSequencia
+public class OrdenaSequencia {
+    
+}

ac5725d8364b36f64e76a1f9b63b302ba14a054e
Wagner Christos <88002748+wagnerchr@users.noreply.github.com>
2022-10-04 17:04:44 -0300

    Add files via upload

---
+++ hold/Avaliativa01/src/avaliativa01/Estado.java
@@ -0,0 +1,23 @@
+package avaliativa01;
+
+public class Estado {
+
+    private String nome;
+    private String sigla;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+}

69f60f457ecc8cf88d50b374841cafc973e31e1e
Wagner Christos <88002748+wagnerchr@users.noreply.github.com>
2022-10-30 12:12:17 -0300

    Add files via upload

---
+++ project-commitments/Compromissos2/src/compromissos2/Compromissos.java
@@ -0,0 +1,36 @@
+package compromissos2;
+
+import view.Login;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Compromissos {
+
+     public static void main(String[] args) {
+        new Login();
+               
+        System.out.println("Hello World!");
+    }
+     
+    // CONEXÃO COM MYSQL
+    public static Connection getConnection() throws SQLException {
+        
+        Connection conn = null;
+        
+        try {
+            
+            String url = "jdbc:mysql://localhost/compromissos?user=root&password=123456";
+            conn = DriverManager.getConnection(url);
+            
+        } catch (SQLException ex) {         
+            throw new SQLException(ex.getMessage());
+        } 
+        
+        return conn;
+    }
+    
+    
+}
+   
+

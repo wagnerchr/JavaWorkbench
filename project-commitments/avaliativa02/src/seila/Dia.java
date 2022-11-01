be7ba3456ff60d073805d8dbc9032ddea6088598
Wagner Christos <88002748+wagnerchr@users.noreply.github.com>
2022-10-31 21:47:12 -0300

    just to hold

---
+++ project-commitments/avaliativa02/src/seila/Dia.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package seila;
+
+import java.awt.Color;
+import javax.swing.JButton;
+
+public class Dia extends JButton {
+    
+    
+    
+    public Dia() {
+    
+        this.setBackground(Color.blue);
+        this.setForeground(Color.white);
+        
+        
+        }
+    }
+    
+    
